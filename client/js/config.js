aeddeba4f89b93f2ef951511dd7b7f39ac9dc4f8
Kenny Inthirath <kenny.inthirath@gmail.com>
2017-07-03 02:42:57 -0700

    Cleaning config

---
+++ client/js/config.js
@@ -0,0 +1,13 @@
+export default {
+    server : {
+        host : '',
+        port : 5001
+    },
+    audioProfile: {
+        SampleRate: 22050,
+        Channels: 1,
+        AudioQuality: "Low",
+        AudioEncoding: "aac",
+        AudioEncodingBitRate: 32000
+    }
+}
